b13c64a62041aa7a9d738e5100a668d96eb5cfe5
Kaan Ersoy <57914492+kaanersoy12@users.noreply.github.com>
2020-11-27 02:29:00 +0300

    Update Body.vue

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -25,7 +25,7 @@ export default {
 </script>
 
 <style>
-.container{max-width: 1400px; width: 80%;margin: 0 auto;margin-bottom: 100px;}
+.container{max-width: 1400px; width: 80%;margin: 0 auto;padding-bottom: 100px;}
 .gifs{ column-count: 4; display: block;}
 .gifs li{list-style: none; margin-bottom: 30px;}
 .gifs li img{width: 100%;}
